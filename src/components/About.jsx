237722cbf33ee4fb4840da1e11e4727953d4ed2a
Mohammed Basar <hamdaanbasar@gmail.com>
2024-04-16 16:15:20 -0500

    About commit

---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Engineer's Insight.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+      Hey there! I'm all about making things happen! Armed with a degree in
+      Computer Science, I'm on a mission to conquer the world of tech. My
+      journey through academia has not only given me a rock-solid foundation
+      but also equipped me with the skills needed to dive headfirst into
+      groundbreaking projects. I've got a knack for Full-stack development
+      and Machine Learning, and I'm itching to put my expertise to the test,
+      tackling the toughest challenges and driving innovation like never
+      before. I thrive on collaboration and soaking up new knowledge like
+      a sponge. If you're on the lookout for someone to shake things up
+      and deliver game-changing results, look no further! Let's team up
+      and make magic happen!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
